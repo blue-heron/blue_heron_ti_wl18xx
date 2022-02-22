4e6159ab26e604c89e9b923a23103a1814d6d65e
Connor Rigby <connorr@hey.com>
2022-02-22 12:22:52 -0700

    Cleanup

--- lib/wilink_8.ex
+++ lib/wilink_8.ex
@@ -3,15 +3,16 @@ defmodule BlueHeron.Wilink8 do
 
   def init(path, gpio_bt_en, tty) do
     bts = BlueHeron.BTS.decode_file!(path)
+    Circuits.UART.find_pids() |> Enum.each(fn {pid, _} -> Circuits.UART.stop(pid) end)
     {:ok, pid} = Circuits.UART.start_link()
     {:ok, pin_ref} = Circuits.GPIO.open(gpio_bt_en, :output)
     Circuits.GPIO.write(pin_ref, 0)
-    Process.sleep(5)
+    Process.sleep(500)
     Circuits.GPIO.write(pin_ref, 1)
     Process.sleep(200)
 
     opts = [
-      speed: 115200,
+      speed: 115_200,
       framing: BlueHeronTransportUART.Framing,
       active: false,
       flow_control: :hardware
@@ -19,35 +20,51 @@ defmodule BlueHeron.Wilink8 do
 
     :ok = Circuits.UART.open(pid, tty, opts)
 
-    state = %{pin_ref: pin_ref, pid: pid, tty: tty, opts: opts}
-    upload(bts.actions, state)
+    state = %{pin_ref: pin_ref, pid: pid, tty: tty, opts: opts, baud: 115_200, flow: 1}
+    :ok = Circuits.UART.write(pid, <<1, 1, 10, 0>>)
+
+    case Circuits.UART.read(pid, 5000) do
+      {:ok, ""} ->
+        {:error, :no_response}
+
+      {:ok, <<_, _, _, _, 1, 10, _::binary>>} ->
+        Logger.info("starting firmware upload")
+        upload(bts.actions, state)
+    end
   end
 
-  def upload([%{type: :action_remarks, data: remark} | rest], state) do
-    Logger.info("TEXAS Remark: #{remark}")
-    upload(rest, state)
-  end
-
-  def upload([%{type: :action_send_command, data: <<1, _::binary>> = packet} | rest], state) do
-    Logger.debug("sending HCI packet: #{inspect(packet, base: :hex, limit: :infinity)}")
-    :ok = Circuits.UART.write(state.pid, packet)
+  def upload([%{type: :action_remarks, data: _remark} | rest], state) do
     upload(rest, state)
   end
 
   def upload(
-        [%{type: :action_wait_event, data: %{msec: timeout, wait_data: wait_data}} | rest],
+        [
+          %{type: :action_send_command, data: <<1, 64780::little-size(16), _::binary>> = _packet}
+          | rest
+        ],
         state
       ) do
-    case Circuits.UART.read(state.pid, timeout) do
-      {:ok, ^wait_data} ->
-        upload(rest, state)
+    Logger.warn("Reached deep sleep command. ")
+    upload(rest, state)
+  end
 
-      {:ok, bad} ->
-        Logger.error(%{
-          unexpected_bts_data: inspect(bad, base: :hex, limit: :infinity),
-          expected: inspect(wait_data, limit: :infinity, base: :hex)
-        })
+  def upload(
+        [
+          %{type: :action_send_command, data: <<1, _opcode::little-size(16), _::binary>> = packet}
+          | rest
+        ],
+        state
+      ) do
+    :ok = Circuits.UART.write(state.pid, packet)
 
+    case Circuits.UART.read(state.pid, 1024) do
+      {:ok, bin} when byte_size(bin) < 7 ->
+        {:error, "TI Init command failed"}
+
+      {:ok, <<_, _, _, _, _, _, resp, _::binary>>} when resp != 0 ->
+        {:error, "TI Init command failed"}
+
+      {:ok, _} ->
         upload(rest, state)
 
       error ->
@@ -55,24 +72,32 @@ defmodule BlueHeron.Wilink8 do
     end
   end
 
-  def upload([%{type: :action_serial, data: %{baud: _baud, flow: _flow}} | rest], state) do
+  def upload(
+        [%{type: :action_wait_event, data: %{msec: _timeout, wait_data: _wait_data}} | rest],
+        state
+      ) do
     upload(rest, state)
-    # Logger.warn("changing baud: #{baud} #{flow}")
-    # updated_opts = Keyword.put(state.opts, :speed, baud)
-    # # :ok = Circuits.UART.configure(state.pid,updated_opts)
-    # :ok =
-    #   Circuits.UART.configure(state.pid,
-    #     speed: 3_000_000,
-    #     framing: BlueHeronTransportUART.Framing,
-    #     active: false,
-    #     flow_control: :hardware
-    #   )
-    # upload(rest, %{state | opts: updated_opts})
+  end
+
+  def upload([%{type: :action_serial, data: %{baud: baud, flow: flow}} | rest], state) do
+    Logger.warn("texas: changing baud rate to #{baud}, flow control to #{flow}")
+    updated_opts = Keyword.put(state.opts, :speed, baud)
+    Circuits.UART.flush(state.pid, :both)
+
+    :ok =
+      Circuits.UART.configure(state.pid,
+        speed: baud,
+        framing: BlueHeronTransportUART.Framing,
+        active: false,
+        flow_control: :hardware
+      )
+
+    upload(rest, %{state | opts: updated_opts})
   end
 
   def upload([], state) do
-    # :ok = Circuits.UART.close(state.pid)
-    # Process.sleep(1000)
+    Circuits.UART.flush(state.pid, :both)
+
     %BlueHeronTransportUART{
       device: state.tty,
       uart_pid: state.pid,
